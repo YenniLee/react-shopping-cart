01002b807f442e7bcda13c14359e76c49aacf2be
Jennie Van <missjenvan@gmail.com>
2019-11-04 16:07:11 -0800

    wip localstorage

--- src/contexts/CartContext.js
+++ src/contexts/CartContext.js
@@ -1,3 +1,25 @@
-import { createContext } from 'react';
+import React, { createContext } from 'react';
+import { useLocalStorage } from '../hooks/useLocalStorage';
 
 export const CartContext = createContext();
+
+export const CartContextProvider = props => {
+    const [cart, setCart] = useLocalStorage("cartItems", []);
+
+	const addItem = item => {
+		setCart([...cart, item]);
+    };
+
+    const removeItem = itemToRemove => {
+        const filteredCart = cart.filter(cartItem => {
+            return itemToRemove.id !== cartItem.id;
+        })
+
+        setCart(filteredCart);
+    }
+    return (
+        <CartContext.Provider value={{ cart, setCart, addItem, removeItem }}>
+            {props.children}
+        </CartContext.Provider>
+    );
+}
